dece5568687823ad5557602011eb4cdaecc4bb73
Ashfaq Nisar <ashfaqnisar00@gmail.com>
2019-09-16 13:26:47 +0530

    1. Created the product-list component and created the table to display the content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
+import {ProductListComponent} from './product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent//Declaring all the components
+    AppComponent,
+    ProductListComponent//Declaring all the components
   ],
   imports: [
     BrowserModule//Application runs correctly in the browser
